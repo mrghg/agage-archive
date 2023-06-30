7f508851497a0ceefbcf42bb2c10584306c2cb8d
Matt Rigby <matt.rigby@bristol.ac.uk>
2023-06-30 16:25:07 +0100

    Updated setup file to default to internal ALE/GAGE files

--- agage_archive/util.py
+++ agage_archive/util.py
@@ -1,16 +1,26 @@
 import configparser
+from pathlib import Path
 
 from agage_archive import get_path
 
 
 def setup():
+    """ Setup the config file for the agage_archive package. """
 
     config = configparser.ConfigParser()
 
+    # Get path to ALE/GAGE data included in repo
+    repo_path = Path(__file__).parents[1].absolute()
+    ale_default = repo_path / "data/ale_gage_sio1993/ale"
+    gage_default = repo_path / "data/ale_gage_sio1993/gage"
+
+
     # Paths to data files
     agage_path = input("Path to folder containing AGAGE GCWerks NetCDF files:")
-    ale_path = input("Path to folder containing ALE tar.gz files:")
-    gage_path = input("Path to folder containing GAGE tar.gz files:")
+    ale_path = input(f"Path to folder containing ALE tar.gz files " + \
+                     f"(press enter for default {ale_default}): ") or ale_default
+    gage_path = input("Path to folder containing GAGE tar.gz files:"  + \
+                     f"(press enter for default {gage_default}): ") or gage_default
     output_path = input("Path to output folder:")
 
     config["Paths"] = {"agage_path": agage_path,
