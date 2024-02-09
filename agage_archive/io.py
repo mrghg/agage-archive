5d3b7af7b32e7b2ae15cdba5479d406c3121ae7f
Ben <qq23840@bristol.ac.uk>
2024-02-09 15:26:27 +0000

    add lines in io.py to drop duplicates (affecting picarro data, apparently)

--- agage_archive/io.py
+++ agage_archive/io.py
@@ -177,6 +177,12 @@ def read_nc(network, species, site, instrument,
                                site=site)
     ds = ds.sel(time=slice(None, rs))
 
+    # Check that time is monotonic and that there are no duplicate indices
+    if not ds.time.is_monotonic_increasing:
+        ds.sortby("time", inplace=True)
+    if len(ds.time) != len(ds.time.drop_duplicates()):
+        ds.drop_duplicates("time", inplace=True)
+
     # If baseline is True, return baseline dataset
     if baseline:
         return ds["baseline"].copy(deep=True).to_dataset(name="baseline")
