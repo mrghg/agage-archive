35d98d9128fe6d124cc55f0ea16dbcd241ebe476
Matt Rigby <matt.rigby@bristol.ac.uk>
2024-06-26 13:59:07 +0000

    Added some error checking if end of release schedule is after start date

--- agage_archive/io.py
+++ agage_archive/io.py
@@ -947,6 +947,10 @@ def output_dataset(ds, network,
     # Select time slice
     ds_out = ds_out.sel(time=slice(None, end_date))
 
+    if len(ds_out.time) == 0:
+        raise ValueError(f"No data retained for {ds_out.attrs['species']} {ds_out.attrs['site_code']} {instrument} after applying release schedule end dates. " + \
+                        "Check dates in release schedule or omit this instrument.")
+
     output_write(ds_out, out_path, filename,
                 output_subpath=output_subpath, verbose=verbose)
 
