36cc195d388da37797b095b92d97276446440960
Matt Rigby <matt.rigby@bristol.ac.uk>
2024-07-05 09:19:27 +0000

    Running for whole archive

--- agage_archive/run.py
+++ agage_archive/run.py
@@ -619,7 +619,7 @@ if __name__ == "__main__":
     print("####################################")
     print("#####Processing public archive######")
     print("####################################")
-    run_all("agage", species=["cfc-11"], public=True)
+    run_all("agage", public=True)
 
     # print("####################################")
     # print("#####Processing private archive#####")
