8b443b60edf42f77003960bd1769ba357bd1f275
Matt Rigby <matt.rigby@bristol.ac.uk>
2024-11-26 16:51:43 +0000

    Resetting run script

--- agage_archive/run.py
+++ agage_archive/run.py
@@ -667,7 +667,7 @@ if __name__ == "__main__":
     print("####################################")
     print("#####Processing public archive######")
     print("####################################")
-    run_all("agage", species = ["cfc-11"], sites = ["ADR"], public=True)
+    run_all("agage", public=True)
 
     # print("####################################")
     # print("#####Processing private archive#####")
