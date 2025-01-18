bb5ee2eb4d366c5dec34b96b79046ec9b6788079
joe-pitt <joseph.pitt@bristol.ac.uk>
2025-01-18 22:02:57 +0000

    set resample=false

--- agage_archive/run.py
+++ agage_archive/run.py
@@ -675,7 +675,7 @@ if __name__ == "__main__":
     print("####################################")
     print("#####Processing public archive######")
     print("####################################")
-    run_all("agage", public=True)
+    run_all("agage", public=True, resample=False)
 
     # print("####################################")
     # print("#####Processing private archive#####")
